47501c7784921bb174277a0a54e2577193b542e4
tiago <si.tiagoferreira@gmail.com>
2018-04-01 21:25:19 -0300

    remove unused imports

--- src/main/java/br/com/game/of/thrones/controller/AffilliateAccountController.java
+++ src/main/java/br/com/game/of/thrones/controller/AffilliateAccountController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
